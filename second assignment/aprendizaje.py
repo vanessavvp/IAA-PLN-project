0000b804060746233614ecc8084ab6288204f3f9
Vanessa Villalba <alu0101265704@ull.edu.es>
2021-05-05 18:04:17 +0100

    Dictionaries done

--- second assignment/aprendizaje.py
+++ second assignment/aprendizaje.py
@@ -16,28 +16,84 @@ import string
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 
-def reading_file(input_file):
-  with open(input_file) as csv_file:
-    punctuation_marks = string.punctuation + '…' + '”' + '“' + '-' + '‘'+ '’' + '´' + '—'
-    csv_reader = csv.reader(csv_file, delimiter=',')
-    list_of_rows = list(csv_reader)
-    print(len(list_of_rows))
-    text = 'Numero de documentos del corpus: ' + str(len(list_of_rows)) + '\n'
-    writing_file('aprendizajeH.txt', text)
-    writing_file('aprendizajeB.txt', text)
-    writing_file('aprendizajeC.txt', text)
-    writing_file('aprendizajeE.txt', text)
 
+class_b, class_c, class_e, class_h = {}, {}, {}, {}
+
+def reading_file(input_file):
+  with open(input_file, "r", encoding = 'utf-8-sig') as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=',')
+    lines = list(csv_reader)
+    return lines
+    
+
+def preprocessing_words(entire_line):
+  punctuation_marks = string.punctuation + '…' + '”' + '“' + '-' + '‘'+ '’' + '´' + '—'
+  data = entire_line.translate(str.maketrans(dict.fromkeys(punctuation_marks, ' ')))
+  en_stops = set(stopwords.words('english'))
+  output_list = set()
+  single_words = data.split()
+  for word in single_words:
+    # Only alphabetics words will be processed
+    if (word.isalpha()):
+      word = word.lower()
+      # Stopwords will be ignored
+      if (word not in en_stops):
+        output_list.add(word)
+  output_list = sorted(output_list)
+  return output_list
+
+
+def initialize_dictionaries(lines):
+  for i in range(len(lines)):
+    words = preprocessing_words(lines[i][1])
+    for word in words:
+      if (lines[i][0] == 'Books'): 
+        if (word in class_b):
+          class_b[word] += 1
+        else:
+          class_b[word] = 1
+      if (lines[i][0] == 'Clothing & Accesories'):
+        if (word in class_c):
+          class_c[word] += 1
+        else:
+          class_c[word] = 1
+      if (lines[i][0] == 'Electronics'):
+        if (word in class_e):
+          class_e[word] += 1
+        else:
+          class_e[word] = 1
+      if (lines[i][0] == 'Household'):
+        if (word in class_h):
+          class_h[word] += 1
+        else:
+          class_h[word] = 1
+
+  
+def corpus_documents_number(lines):
+  text = 'Numero de documentos del corpus: ' + str(len(lines)) + '\n'
+  writing_file('aprendizajeH.txt', text)
+  writing_file('aprendizajeB.txt', text)
+  writing_file('aprendizajeC.txt', text)
+  writing_file('aprendizajeE.txt', text)
+
+
+def corpus_words_number(file_name):
+  print('hola')
+  # data = read_and_tokenize(file_name)
+  # preprocessing_words(data)
 
 def writing_file(file_name, text):
   new_file = open(file_name,"wt")
   new_file.write(text)
-  print("The file was successfully written! into \"" + file_name + "\" file\n")
+  print("\nThe file was successfully written! into \"" + file_name + "\" file")
 
 
 def main():
   file = 'ecom-train.csv'
-  reading_file(file)
+  lines = reading_file(file)
+  corpus_documents_number(lines)
+  initialize_dictionaries(lines)
+  #corpus_words_number(file)
   
 
 main()
---
+++ second assignment/vocabulary.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python3
+"""
+Created on Thursday April 22, 2021
+
+@author: Vanessa Valentina Villalba Pérez
+Natural language processing project PLN 1º part
+
+"""
+import re
+import csv
+import nltk
+import string
+
+nltk.download('stopwords')
+nltk.download('punkt')
+
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+
+
+def read_and_tokenize(input_file):
+  with open(input_file, "r", encoding = 'utf-8-sig') as csv_file:
+    punctuation_marks = string.punctuation + '…' + '”' + '“' + '-' + '‘'+ '’' + '´' + '—'
+    csv_reader = csv.reader(csv_file, delimiter=',')
+    list_of_rows = list(csv_reader)
+    data = []
+    for line in list_of_rows:
+      # Every line will be tokenized into a word without taking into account the punctuation marks
+      data += word_tokenize(line[1].translate(str.maketrans(dict.fromkeys(punctuation_marks, ' '))))
+    return data
+
+
+def preprocessing_words(data):
+  en_stops = set(stopwords.words('english'))
+  output_list = set()
+  for word in data:
+    # Only alphabetics strings will be processed
+    if (word.isalpha()):
+      word = word.lower()
+      # Stopwords will be ignored
+      if (word not in en_stops):
+        output_list.add(word)
+  output_list = sorted(output_list)
+  return output_list
+
+
+def writing_file(output_list, vocabulary_file):
+  total_words = len(output_list)
+  new_file = open(vocabulary_file,"wt")
+  new_file.write("Número de palabras: " + str(total_words) + "\n")
+  for word in output_list:
+    new_file.write(word + "\n")
+  print("The file was successfully written! into \"" + vocabulary_file + "\" file")
+
+
+def main():
+  print("\nLoading...\n")
+  input_file = 'ecom-train.csv'
+  output_file = 'vocabulario.txt'
+  data_list = read_and_tokenize(input_file)
+  final_list = preprocessing_words(data_list)
+  writing_file(final_list, output_file)
+
+
+main()
+
+
+
