4413bf6c50a5a5a3ae92115ca9e071f07f42970b
Vanessa Villalba <alu0101265704@ull.edu.es>
2021-05-07 19:31:42 +0100

    Adding new files

---
+++ second assignment/clasificacion.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Created on Friday May 7, 2021
+
+@author: Vanessa Valentina Villalba Pérez
+Natural language processing project PLN final part
+"""
+import re
+import csv
+import math
+import nltk
+import string
